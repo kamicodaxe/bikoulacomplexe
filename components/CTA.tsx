ce1320f4969ed665dbdfb23d85bd945ae5551108
KAMI Loïc <ruvice.kami@gmail.com>
2022-07-27 03:02:01 +0100

    - Updated components: CTA, Footer and, contact page
    TIME: 1H

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,12 +3,12 @@
 const CTA: React.FC<{}> = ({ }) => {
 
     return (
-        <section className="py-6 text-gray-800">
+        <section className="py-6 bg-yellow-800 text-gray-100">
             <div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
                 <h1 className="text-3xl font-semibold leading-tight text-center lg:text-left">
                     Vous souhaitez collaborer avec nous?
                 </h1>
-                <button className="px-8 py-3 text-lg font-semibold rounded bg-brown text-gray-100">
+                <button className="px-8 py-3 text-lg font-semibold rounded text-brown bg-gray-100">
                     Prénez un rdv!
                 </button>
             </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,8 +22,8 @@ const Contact: NextPage = () => {
     <Layout>
       <NavBar active="contact" />
       <Header title="Contact Us" />
-      {/* <section className="py-6 dark:bg-gray-800 dark:text-gray-50"> */}
-      <section className="py-6">
+      <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
+        {/* <section className="py-6"> */}
         <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
           <div className="py-6 md:py-0 md:px-6">
             <h1 className="text-4xl font-bold">Get in touch</h1>
@@ -50,20 +50,23 @@ const Contact: NextPage = () => {
               </p>
             </div>
           </div>
-          <form novalidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
+          <form noValidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
             <label className="block">
               <span className="mb-1">Full name</span>
-              <input type="text" placeholder="Leroy Jenkins" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:bg-gray-800" />
+              <input
+                type="text"
+                placeholder="Leroy Jenkins"
+                className="block w-full rounded-md border-b border-white focus:outline-none focus:border-brown bg-gray-800" />
             </label>
             <label className="block">
               <span className="mb-1">Email address</span>
-              <input type="email" placeholder="leroy@jenkins.com" className="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:bg-gray-800" />
+              <input type="email" placeholder="leroy@jenkins.com" className="block w-full rounded-md border-b border-white focus:outline-none focus:border-brown bg-gray-800" />
             </label>
             <label className="block">
               <span className="mb-1">Message</span>
-              <textarea rows="3" className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:bg-gray-800"></textarea>
+              <textarea rows={5} className="block w-full rounded-md border-b border-white focus:outline-none focus:border-brown bg-gray-800"></textarea>
             </label>
-            <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-violet-400 dark:text-gray-900 focus:ring-violet-400 hover:ring-violet-400">Submit</button>
+            <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-violet-400 dark:text-gray-900 focus:ring-brown hover:ring-violet-400">Submit</button>
           </form>
         </div>
       </section>

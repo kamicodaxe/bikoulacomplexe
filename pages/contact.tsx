cfda289872cf43f20fa0d8e00e403719a1cb366f
KAMI Loïc <ruvice.kami@gmail.com>
2022-08-17 22:19:31 +0100

    - updated contact page
    TIME: 0.15H

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames'
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
 import NavBar from '../components/NavBar'
@@ -18,6 +20,8 @@ import NavBar from '../components/NavBar'
 // }
 
 const Contact: NextPage = () => {
+  const [tabIndex, setTabIndex] = useState(0)
+  const contactInfo = CONTACT_INFOS[tabIndex]
   return (
     <Layout>
       <NavBar active="contact" />
@@ -28,26 +32,43 @@ const Contact: NextPage = () => {
           <div className="py-6 md:py-0 md:px-6">
             <h1 className="text-4xl font-bold">Contactez-nous</h1>
             <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+            <div className="container max-w-xl p-6 pt-12 mx-auto space-y-8 lg:px-8 lg:max-w-7xl">
+              <div className="flex justify-center items-center">
+                {
+                  [
+                    "Cameroun",
+                    "France",
+                  ].map((title, index) => (
+                    <span
+                      key={title}
+                      onClick={() => setTabIndex(index)}
+                      className={classNames([
+                        "px-5 border-b-2 border-gray-700 hover:cursor-pointer text-lg",
+                        tabIndex === index && "border-b-[#5D8B84] text-[#5D8B84]",
+                      ])}>{title}</span>
+                  ))
+                }
+              </div>
+            </div>
             <div className="space-y-4">
               <p className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
                 </svg>
-                <span>15 Rue Antoine de Saint-Exupéry</span>
-                <span>94270 Le Kremlin-Bicêtre, France</span>
+                {contactInfo.address.map(title => <span key={title}>{title}</span>)}
               </p>
               <p className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                 </svg>
-                <span>06 63 87 54 46</span>
+                <span>{contactInfo.tel}</span>
               </p>
               <p className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                 </svg>
-                <span>info@bikoulacomplexe.com</span>
+                <span>{contactInfo.email}</span>
               </p>
             </div>
           </div>
@@ -76,5 +97,24 @@ const Contact: NextPage = () => {
     </Layout>
   )
 }
+
+const CONTACT_INFOS = [
+  {
+    address: [
+      "ligne #1",
+      // "ligne #2"
+    ],
+    tel: "(+237) ### ### ###",
+    email: "info@bikoulacomplexe.com"
+  },
+  {
+    address: [
+      "15 Rue Antoine de Saint-Exupéry",
+      "94270 Le Kremlin-Bicêtre, France"
+    ],
+    tel: "06 63 87 54 46",
+    email: "info@bikoulacomplexe.com"
+  }
+]
 
 export default Contact

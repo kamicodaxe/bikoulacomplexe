381cb29a9f1331f82bc55efdca9ec668996f9bd8
KAMI Loïc <ruvice.kami@gmail.com>
2022-09-02 20:43:44 +0100

    - Updated about us

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -11,12 +11,12 @@ const AboutUs: React.FC<{}> = ({ }) => {
                     <Image width="300px" height="300px" src={logo} alt="Logo Bikoula Complexe" />
                 </div>
                 <div className="flex flex-1 flex-col justify-center align-center">
-                    <h2 className='text-4xl pb-8 font-semibold text-gray-800'>Des opportunité en Afrique</h2>
+                    <h2 className='text-4xl pb-8 font-semibold text-gray-800'>Des opportunités en Afrique</h2>
                     <p className='text-lg text-gray-600'>
-                        Nous sommes le lien entre l’Afrique et l’Occident pour toutes les entreprises désireuses de s’implanter sur le continent africain. Nous vous garantissons la mise en relation et la meilleure approche avec les acteurs économiques privés et publics. Vous bénéficiez d’un carnet d’adresse fiable pour la réussite de vos projets. Nous vous accompagnons dans la recherche des financements, des potentiels investisseurs et groupes d’investisseurs à obtenir des financements auprès des instituts financières. Nous sommes notamment spécialiste de l’Afrique dans l’accompagnement et le conseil du secteur public et privé et des porteurs de projets.
-                        {/* Notre société est d’abord basée sur notre relation avec des entreprises étrangères.
+                    Nous sommes le lien entre l&apos;Afrique et l&apos;Occident pour toutes les entreprises désireuses de s&apos;implanter sur le continent africain. Nous vous garantissons la mise en relation et la meilleure approche avec les acteurs économiques privés et publics. Vous bénéficiez d&apos;un carnet d&apos;adresse fiable pour la réussite de vos projets. Nous vous accompagnons dans la recherche des financiers, des potentiels investisseurs et groupes d&apos;investissement à obtenir des financements auprès des instituts financiers. Nous sommes notamment spécialistes de l&apos;Afrique dans l&apos;accompagnement et le conseil du secteur public et privé et des porteurs de projets.
+                        {/* Notre société est d&apos;abord basée sur notre relation avec des entreprises étrangères.
                         Notre fonction et notre position issu de la diaspora est de leur apporter des informations fiables,
-                        actualisées et de les conseiller en matière d’investissements... */}
+                        actualisées et de les conseiller en matière d&apos;investissements... */}
                     </p>
                     {/* <div className="flex flex-col mt-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                         <Link href="/contact" className="px-8 py-3  rounded text-brown">

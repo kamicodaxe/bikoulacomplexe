ffec8140ea25357471516edb214ff2e19bd56874
KAMI Loïc <ruvice.kami@gmail.com>
2022-09-29 21:15:29 +0100

    - updated components/AboutCEO.tsx
    TIME: 1H

--- components/AboutCEO.tsx
+++ components/AboutCEO.tsx
@@ -14,8 +14,11 @@ const AboutCEO: React.FC<{}> = ({ }) => {
                                 <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                                 <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
                             </svg>
-                            
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.
+
+                            Nous concevons le monde comme étant un grand réseau planétaire inter connecté, la société Groupe Bikoula Complexe SARL permet L’interaction entre les atomes et les hémisphères.
+                            Nous sommes une plateforme qui apporte aussi le conseil, l’accompagnement aux entrepreneurs venant de divers horizons dans des domaines d’activités différents.
+                            Nombreux sont ceux d’entre vous ont effleurés l’idée d’aller entreprendre vers d’autres cieux.
+                            La société GBC SARL vous apporte la solution vers ce monde qui vous semble lointain, mais aussi proche de vous. La géopolitique mondiale est une équation à plusieurs variantes, qui va nous obliger à penser différemment, changer nos habitudes et nos rapports avec les autres. Nous vous garantissons une approche différente dans la recherche des partenariats stratégiques en sortant des sentiers battus avec le concours d’une équipe jeune, atypique et dynamique.
                             
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 text-[#5D8B84]">
                                 <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>

e4fae640d8b712d44d367cb0d8b8917be7941ffc
len-kami <lkami@bogital.com>
2022-12-31 12:53:46 +0100

    - updated formsubmit email address

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -74,7 +74,7 @@ const Contact: NextPage = () => {
           </div>
           <form
             className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid"
-            action="https://formsubmit.co/info@bikoulacomplexe.com" method="POST"
+            action="https://formsubmit.co/engonojeanluc@bikoulacomplexe.com" method="POST"
           >
             <label className="block">
               <span className="mb-1">Noms et Prénoms</span>

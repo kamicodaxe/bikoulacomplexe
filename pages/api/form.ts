32db7aeda823cd04bc7c5d0841511c8a057e2493
len-kami <lkami@bogital.com>
2022-12-31 16:38:31 +0100

    - fixed forms api

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -29,7 +29,7 @@ export default async function handler(
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: `"Bogital Forms" <${req.body._email}>`,
-        to: "lkami@bogital.com",
+        to: req.body._email,
         subject: req.body._subject,
         text: "",
         html: `

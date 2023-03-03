00f5c6dd0ab9e640bd7978a996882ac8cf39ec59
len-kami <lkami@bogital.com>
2023-03-03 16:16:32 +0100

    - Added Google Analytics Tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Footer from "./Footer"
 
 
@@ -37,6 +38,24 @@ const Layout: React.FC<Props> = ({ children }) => {
             </Head>
             {children}
             <Footer />
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
+                }}
+            />
         </>
     )
 }

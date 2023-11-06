6b8d5a7390ff22c4269d5ea41f535d0e842caf57
Loïc Kami <lkami@bogial.com>
2023-11-06 22:14:40 +0100

    Update HomeHeader

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -10,18 +10,15 @@ const HomeHeader: React.FC<{}> = ({ }) => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             if (!headerRef.current) return
-            console.log(headerRef.current)
             const mouseenter = () => {
                 setIsInView(true)
-                console.log('Mouse enter')
             }
-            headerRef.current.addEventListener('mouseenter', mouseenter)
+            headerRef.current.addEventListener('mousemove', mouseenter)
             const mouseleave = () => {
-                setIsInView(false)
-                console.log('Mouse leave')
+                setTimeout(() => setIsInView(false), 3000)
             }
             headerRef.current.addEventListener('mouseleave', mouseleave)
-            setTimeout(() => setIsInView(false), 5000)
+
             return () => {
                 headerRef.current && headerRef.current.removeEventListener('mouseenter', mouseenter)
                 headerRef.current && headerRef.current.removeEventListener('mouseleave', mouseleave)
@@ -32,30 +29,41 @@ const HomeHeader: React.FC<{}> = ({ }) => {
     return (
         // @ts-ignore
         <section className="bg-gray-800 text-gray-100" ref={headerRef}>
-            <div className="container flex flex-col justify-center p-6 lg:flex-row lg:justify-between">
-                <div className="flex flex-col justify-start p-4 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                    <h1 className="text-xl font-bold leading-none sm:text-6xl">
-                        Votre consultant pour vos activités en
-                        <span className="text-brown"> Afrique</span>
-                    </h1>
-                    {/* <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-                        <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
-                    </p> */}
+            <div className="container flex flex-col p-6 mx-auto sm:py-12 lg:py-12 lg:flex-row">
+
+                <div className="flex flex-col justify-center text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+
+                    <div className="w-full space-y-4">
+                        <h1 className="text-xl font-bold leading-none sm:text-4xl md:text-5xl w-full">
+                            Votre consultant pour vos activités en
+                            <span className="text-brown"> Afrique</span>
+                        </h1>
+                        <p>
+                            Façonnez l&apos;avenir de vos projets d&apos;entreprise en Afrique avec nous. Partenaire incontournable,
+                            Bikoula Complexe International Consulting vous guide vers le succès. Réservez votre rendez-vous
+                            dès maintenant pour concrétiser vos ambitions entrepreunariale en Afrique.
+                        </p>
+                    </div>
+
                     <div className="flex flex-col mt-8 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:mt-16 lg:justify-start">
                         <Link href='/contact'>
                             <span className="px-8 py-3 text-lg font-semibold rounded bg-brown text-white hover:cursor-pointer">
                                 Réserver un rdv
                             </span>
                         </Link>
+                        <button>
+                            <Link href="/projets">
+                                <span className="px-8 py-3 text-lg font-semibold rounded border-2 border-brown text-brown hover:cursor-pointer">
+                                    Nos réalisations
+                                </span>
+                            </Link>
+                        </button>
                     </div>
 
                 </div>
+
                 <div className="flex bg-transparent w-full overflow-hidden relative">
-                    {/* @ts-ignore */}
-                    {
-                        isInView && <Cobe isVisible={isInView} />
-                        // <Cobe isVisible={isInView} />
-                    }
+                    <Cobe rotate={isInView} />
                 </div>
 
             </div>
